b98b22576ff8ef5d0ee661731ba07e0aa7aad51c
Abdalla2200 <abdallaessam3300@gmail.com>
2024-12-12 23:06:07 +0200

    Initial upload of the design

---
+++ assets/JS/tailwindcss.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      container: {
+        center: true, // mx-auto
+        padding: "1rem", // px-4
+      },
+      colors: {
+        customColor: "blue",
+      },
+      fontFamily: {
+        body: ["Open Sans", "serif"],
+        heading: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+};
